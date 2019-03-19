5d0f29b23443bfd568640b57eae4cd137a715273
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-19 06:41:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,6 +27,7 @@ end
 end
 
 def find_the_cheese(array)
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  
 end
+
+cheese_types = ["cheddar", "gouda", "camembert"]
