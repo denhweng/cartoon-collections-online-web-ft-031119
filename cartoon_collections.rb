2557dba08d79a77787c46d66349f1ffe9814061d
Dan Hwang <danhwangfkpc@gmail.com>
2019-03-19 06:36:40 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,7 +20,10 @@ def long_planeteer_calls(array)
   i = 0
   if array.any? {|i| i.length > 4}
     true 
-  else 
+  else
+    false 
+  i += 1 
+end
 end
 
 def find_the_cheese# code an argument here
